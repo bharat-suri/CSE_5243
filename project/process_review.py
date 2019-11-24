1e7ca94222fe66354f5a201e32deceb3452b5b01
bharat.suri <bsuri97@gmail.com>
2019-11-24 18:44:03 -0500

    Added vocabulary

--- project/process_review.py
+++ project/process_review.py
@@ -1,10 +1,15 @@
 import nltk
 from nltk.corpus import stopwords 
 from nltk.tokenize import RegexpTokenizer
+from nltk.stem.porter import PorterStemmer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.utils.extmath import randomized_svd
 import pandas as pd
+from pandas.io.json import json_normalize
 import numpy as np
 import json
 import sys
+import re
 import os
 
 directory = '/Users/bharatsuri/Github/CS_5243/project/data/'
@@ -12,17 +17,23 @@ restaurants = directory + 'restaurants.pkl'
 restaurants_review_json = directory + 'restaurants_review.json'
 restaurants_review_pkl = directory + 'restaurants_review.pkl'
 
-df = pd.read_pickle(restaurants_review_pkl)
+review_df = pd.read_pickle(restaurants_review_pkl)
+review_df = review_df.set_index('review_id')
 
-print (df.head(5))
+restaurant_df = pd.read_pickle(restaurants)
+restaurant_df = restaurant_df.set_index('business_id')
 
+print (review_df.head(5))
+print (review_df.shape)
+print (restaurant_df.head(5))
+print (restaurant_df.shape)
+
+corpus = review_df['stemmed_text'].values
 vocab = set()
-for s in df['text'].values:
-    vocab.update(s)
-
+for s in corpus:
+    vocab.update(s.split())
 print (len(vocab))
-
-groups = df.groupby(df['date'].dt.to_period('Q'))
+groups = review_df.groupby(review_df['date'].dt.to_period('Q'))
 
 for idx, group in groups:
     print (group.shape)
