49b10348d655fff62c2187eefe3868aea05ad633
bharat.suri <bsuri97@gmail.com>
2019-11-24 20:01:00 -0500

    Added SVD

--- project/process_review.py
+++ project/process_review.py
@@ -5,7 +5,6 @@ from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.utils.extmath import randomized_svd
 import pandas as pd
-from pandas.io.json import json_normalize
 import numpy as np
 import json
 import sys
@@ -20,20 +19,77 @@ restaurants_review_pkl = directory + 'restaurants_review.pkl'
 review_df = pd.read_pickle(restaurants_review_pkl)
 review_df = review_df.set_index('review_id')
 
-restaurant_df = pd.read_pickle(restaurants)
-restaurant_df = restaurant_df.set_index('business_id')
+# restaurant_df = pd.read_pickle(restaurants)
+# restaurant_df = restaurant_df.set_index('business_id')
 
 print (review_df.head(5))
-print (review_df.shape)
-print (restaurant_df.head(5))
-print (restaurant_df.shape)
+# print (restaurant_df.head(5))
 
-corpus = review_df['stemmed_text'].values
+corpus = review_df['text'].values
+stemmed_corpus = review_df['stemmed_text'].values
 vocab = set()
-for s in corpus:
+for s in stemmed_corpus:
     vocab.update(s.split())
-print (len(vocab))
-groups = review_df.groupby(review_df['date'].dt.to_period('Q'))
 
-for idx, group in groups:
-    print (group.shape)
+vect = CountVectorizer()
+vect.fit(stemmed_corpus)
+X = vect.transform(stemmed_corpus).toarray()
+
+tf_idf = pd.DataFrame(
+    X.transpose(),
+    index=vect.get_feature_names(),
+    columns=review_df.index
+)
+
+n_concepts = 15
+concepts = [(lambda x: 'concept{:d}'.format(x))(i) for i in range(1,n_concepts+1)]
+U, Sigma, VT = randomized_svd(X, n_components=n_concepts)
+
+U_df = pd.DataFrame(
+    U,
+    index=review_df.index,
+    columns=concepts
+)
+U_df['content'] = corpus
+
+Sigma_df = pd.DataFrame(
+    np.diag(Sigma),
+    index=concepts,
+    columns=concepts
+)
+
+VT_df = pd.DataFrame(
+    VT,
+    index=concepts,
+    columns=vocab
+)
+
+print (U_df)
+print (Sigma_df)
+print (VT_df.T)
+
+
+# groups = review_df.groupby(review_df['date'].dt.to_period('Q'))
+
+# restaurant_features = [
+#     'name',
+#     'latitude',
+#     'longitude',
+#     'stars',
+#     'review_count',
+# ]
+
+# expanded_restaurant = restaurant_df['categories'].str.split(', ', expand=True).stack().str.get_dummies().sum(level=0)
+
+
+# r = 0
+# for idx, row in review_df.iterrows():
+#     b_id = row['business_id']
+#     for c in restaurant_features:
+#         review_df.loc[idx, c] = restaurant_df[c][b_id]
+#     for c in expanded_restaurant:
+#         review_df.loc[idx, c] = expanded_restaurant[c][b_id]
+#     r += 1
+#     print ('At review ', r, end='\r')
+
+# review_df.to_pickle(restaurants_review_pkl)
